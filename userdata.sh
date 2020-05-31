0d9614226f24137bbd7e8eb0e1a8df4327bcb189
dmascheck <53113853+dmascheck@users.noreply.github.com>
2020-05-31 11:49:25 -0500

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-yum update -y
+###add services
 yum install git -y
 yum install httpd -y
 amazon-linux-extras php7.2 -y 
 systemctl enable httpd
 chkconfig httpd
+###copy web files
 git clone https://github.com/dmascheck/octankweb.git
 cd octankweb
 cp * /var/www/html
+###install stress
 amazon-linux-extras install epel -y 
 yum install stress -y
 service httpd restart
