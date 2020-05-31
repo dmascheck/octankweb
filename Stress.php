13ae1b6ac5882a40f6fce5595efeb64a24a74878
dmascheck <53113853+dmascheck@users.noreply.github.com>
2020-05-31 11:40:12 -0500

    Update stress and userdata
    resoled stress issue.  made silent install
    updated userdata to use git

--- Stress.php
+++ Stress.php
@@ -1,9 +1,5 @@
-
-
 <?php
-$output = system('stress --cpu 8 --timeout 120');
-echo $output;
+$output = shell_exec('stress --cpu 8 --timeout 120 > output.txt &');
+echo "<pre>$output</pre>";
+header("Location: http://octanklb-761066200.us-west-1.elb.amazonaws.com/server.php
 ?>
-
-
-
--- userdata.sh
+++ userdata.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 yum update -y
-yum install git -y 
-yum install httpd -y 
+yum install git -y
+yum install httpd -y
 amazon-linux-extras php7.2 -y 
 systemctl enable httpd
 chkconfig httpd
+git clone https://github.com/dmascheck/octankweb.git
+cd octankweb
+cp * /var/www/html
 amazon-linux-extras install epel -y 
 yum install stress -y
 service httpd restart
